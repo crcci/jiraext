0ef114f998687f08e47b904ee74bbc822ca7fa66
sftqa <qa@ugurnurcan.com>
2023-08-23 15:47:54 -0400

    test id extraction, remove duplicate, only upload re-try attachments

--- xmlparser.js
+++ xmlparser.js
@@ -9,7 +9,7 @@ parseString(xmlData, function(err, result) {
     return;
   }
 
-  const failedTestCases = [];
+  const failedTestCases = new Map();
 
   if (result.testsuites && result.testsuites.testsuite) {
     const testSuites = result.testsuites.testsuite;
@@ -19,38 +19,50 @@ parseString(xmlData, function(err, result) {
         for (const testcase of testsuite.testcase) {
           if (testcase.failure && testcase['system-out']) {
             const testName = extractTestName(testcase.$.name);
-            const attachments = [];
 
-            const cdataContent = testcase['system-out'][0];
+            if (!failedTestCases.has(testName)) {
+              const attachments = [];
 
-            if (cdataContent) {
-              const attachmentPaths = cdataContent.match(/\[\[ATTACHMENT\|(.*?)]]/g);
+              const cdataContent = testcase['system-out'][0];
 
-              if (attachmentPaths) {
-                for (const attachmentPath of attachmentPaths) {
-                  const attachmentInfo = {
-                    path: attachmentPath.replace(/\[\[ATTACHMENT\|(.*)]]/, '$1')
-                  };
-                  attachments.push(attachmentInfo);
+              if (cdataContent) {
+                const attachmentPaths = cdataContent.match(/\[\[ATTACHMENT\|(.*?)]]/g);
+
+                if (attachmentPaths) {
+                  for (const attachmentPath of attachmentPaths) {
+                    if (attachmentPath.includes('-retry1')) { // Only include attachments with "-retry1"
+                      const attachmentInfo = {
+                        path: attachmentPath.replace(/\[\[ATTACHMENT\|(.*)]]/, '$1')
+                      };
+                      attachments.push(attachmentInfo);
+                    }
+                  }
                 }
               }
-            }
 
-            failedTestCases.push({ name: testName, attachments: attachments });
+              if (attachments.length > 0) { // Only add to map if there are attachments
+                const testId = extractTestId(testName);
+                failedTestCases.set(testName, { test_id: testId, name: testName, attachments: attachments });
+              }
+            }
           }
         }
       }
     }
   }
 
-  const jsonOutput = JSON.stringify({ failed_test_cases: failedTestCases }, null, 2);
+  const jsonOutput = JSON.stringify({ failed_test_cases: Array.from(failedTestCases.values()) }, null, 2);
 
   fs.writeFileSync('failed_test_cases.json', jsonOutput);
   console.log('JSON file with failed test cases and attachments created.');
 });
 
 function extractTestName(fullName) {
-  // Extract the test name after the last › character and before @
   const matches = fullName.match(/›\s*([^@]+)\s*@/);
   return matches ? matches[1].trim() : fullName;
+}
+
+function extractTestId(testName) {
+  const matches = testName.match(/Test-ID:(\d+)/);
+  return matches ? matches[1] : 'Unknown';
 }
